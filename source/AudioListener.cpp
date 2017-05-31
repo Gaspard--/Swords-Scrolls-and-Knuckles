6eaf56fdc81be38ddc5ba077f7dca27ec6ba2536
melis_m <matteo.melis@epitech.eu>
2017-05-31 05:47:27 +0200

    member functions were not marked as const

--- source/AudioListener.cpp
+++ source/AudioListener.cpp
@@ -8,13 +8,13 @@ AudioListener::AudioListener(Vect<3, float> const &pos)
   setPos(pos);
 }
 
-void AudioListener::setPos(Vect<3, float> const &v)
+void AudioListener::setPos(Vect<3, float> const &v) const
 {
   alListener3f(AL_POSITION, v[0], v[1], v[2]);
   Audio::checkError();
 }
 
-void AudioListener::setVolume(float f)
+void AudioListener::setVolume(float f) const
 {
   alListenerf(AL_GAIN, f);
   Audio::checkError();
