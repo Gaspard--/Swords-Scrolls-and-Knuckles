e8af05dab0818cfa55ed598d64a08e18f63b0838
melis_m <matteo.melis@epitech.eu>
2017-06-09 22:16:43 +0200

    camera x position is ok now (z when moving on x as well), y not yet, code is a bet messy, will probably refactor some things

--- source/Logic.cpp
+++ source/Logic.cpp
@@ -64,7 +64,7 @@ Logic::Logic(LevelScene &levelScene, Renderer &renderer, std::vector<AnimatedEnt
   , projectiles(gameState.projectiles, levelScene.projectiles)
   , entityFactory(renderer)
 {
-  for (unsigned int i(0); i != 2u; ++i) // TODO: obviously players should be passed as parameter or something.
+  for (unsigned int i(0); i != 3u; ++i) // TODO: obviously players should be passed as parameter or something.
     gameState.players.emplace_back(0.5, Vect<2u, double>{5.0, 4.0 + i});
   levelScene.setTerrain(gameState.terrain);
 }
@@ -113,26 +113,6 @@ void Logic::updateDisplay(LevelScene &levelScene)
 		      {
 			entity.setPosition(projectile.pos[0], 0, projectile.pos[1]);
 		      });
-  /*
-    gameState.players[0].setInput(p0 * 3.0);
-    gameState.players[1].setInput(p1 * 3.0);
-    gameState.players[2].setInput(p2 * 3.0);
-    std::cout << "p0 : " << gameState.players[0].getPos() << '\n'
-      << "p1 : " << gameState.players[1].getPos() << '\n'
-      << "p2 : " << gameState.players[2].getPos() << std::endl;
-
-    if (mmx.second - mmx.first > 1500 + levelScene.cameraNode->getPosition().y)
-      {
-	y += mmx.second - mmx.first - 1500 + levelScene.cameraNode->getPosition().y;
-      }
-
-    std::cout << "min : " << mmx.first << ", max : " << mmx.second << std::endl;
-
-      {
-	#include <OgreAxisAlignedBox.h>
-
-      }*/
-
 
   for (unsigned int i(0); i != gameState.players.size(); ++i)
     {
@@ -197,26 +177,49 @@ void Logic::updateDisplay(LevelScene &levelScene)
      p2 += {1.0, 0.0};
      }
 
+
+  static constexpr auto angle = 180 - 60 / 2;
   gameState.players[0].setInput(p0 * 0.03);
   gameState.players[1].setInput(p1 * 0.03);
-    Ogre::Real x(0.0);
-    Ogre::Real y(levelScene.cameraNode->getPosition().y);
+  gameState.players[2].setInput(p2 * 0.03);
 
-    auto minmax_x(std::minmax_element(gameState.players.cbegin(),
-				      gameState.players.cend(),
-				      [](auto &p1, auto &p2) {
-					return p1.getPos()[0] < p2.getPos()[0];
-				      }));
-    auto mmx = std::make_pair(minmax_x.first->getPos()[0], minmax_x.second->getPos()[0]);
-    Ogre::Real z(0.0);
-    std::for_each(gameState.players.cbegin(), gameState.players.cend(),
-		  [&x, &z](auto &p) {
-		    x += p.getPos()[0];
-		    z += p.getPos()[1];
-		  });
-    x /= (float)gameState.players.size();
-    z = z / (float)gameState.players.size() + 0.5 * levelScene.cameraNode->getPosition().y;
+  auto minmax_x(std::minmax_element(gameState.players.cbegin(),
+				    gameState.players.cend(),
+				    [](auto &p1, auto &p2) {
+				      return p1.getPos()[0] < p2.getPos()[0];
+				    }));
+  auto leftVecX = Ogre::Vector3{minmax_x.first->getPos()[0], 0,
+    minmax_x.first->getPos()[1]} - levelScene.cameraNode->getPosition();
+  auto rightVecX = Ogre::Vector3{minmax_x.second->getPos()[0], 0,
+    minmax_x.first->getPos()[1]} - levelScene.cameraNode->getPosition();
+  auto midVecX = (rightVecX - leftVecX) / 2;
 
-    levelScene.cameraNode->setPosition(x, y, z);
+  auto minmax_z(std::minmax_element(gameState.players.cbegin(),
+				    gameState.players.cend(),
+				    [](auto &p1, auto &p2) {
+				      return p1.getPos()[2] < p2.getPos()[2];
+				    }));
+  auto leftVecZ = Ogre::Vector3{minmax_z.first->getPos()[2], 0,
+    minmax_z.first->getPos()[1]} - levelScene.cameraNode->getPosition();
+  auto rightVecZ = Ogre::Vector3{minmax_z.second->getPos()[2], 0,
+    minmax_z.first->getPos()[1]} - levelScene.cameraNode->getPosition();
+  auto midVecZ = (rightVecZ - leftVecZ) / 2;
+
+
+  Ogre::Real y(std::max((-std::tan(angle) * (midVecX.length()) + 10) * 1.5,
+	       (-std::tan(angle) * (midVecZ.length()) + 10) * 1.5));
+  Ogre::Real z(levelScene.cameraNode->getPosition().z);
+  Ogre::Real x(minmax_x.first->getPos()[0]
+	       + (minmax_x.second->getPos()[0] - minmax_x.first->getPos()[0]) / 2);
+
+  /*std::for_each(gameState.players.cbegin(), gameState.players.cend(),
+		[&x, &z](auto &p) {
+		  x += p.getPos()[0];
+		  z += p.getPos()[1];
+		});
+  x /= (float)gameState.players.size();
+  z = z / (float)gameState.players.size() + 0.5 * levelScene.cameraNode->getPosition().y;*/
+
+  levelScene.cameraNode->setPosition(x, y, z);
   updatesSinceLastFrame = 0;
 }
