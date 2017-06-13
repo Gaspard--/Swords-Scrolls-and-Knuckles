8cfe72e493012c0a82b59aceaa5cfea22517da46
benjamin grange <benjamin.grange@epitech.eu>
2017-06-13 21:23:37 +0200

    Added animations for ennemies

--- include/Animations.hpp
+++ include/Animations.hpp
@@ -8,7 +8,9 @@ namespace Animations
   namespace Controllable
   {
     constexpr char const WALK[] = "Walk";
+    constexpr char const WALK_RIDE[] = "Walk_Ride";
     constexpr char const STAND[] = "Stand";
+    constexpr char const STAND_RIDE[] = "Stand_Ride";
     constexpr char const ATTACK[] = "Attack";
     constexpr char const STUN[] = "Stun";
     constexpr char const TAUNT[] = "Taun";
@@ -18,8 +20,18 @@ namespace Animations
     {
       constexpr char const SPELL_A[] = "Spell_A";
     }
-  }
 
+  }
+  namespace Ennemy
+  {
+    constexpr char const STAND[] = "Stand";
+    constexpr char const STUN[] = "Stun";
+    constexpr char const WALK[] = "Walk";
+    constexpr char const DEATH[] = "Death";
+
+    constexpr char const ATTACK_01[] = "Attack";
+    constexpr char const ATTACK_02[] = "Attack_01";
+  };
 };
 
 #endif // !ANIMATIONS_HPP
