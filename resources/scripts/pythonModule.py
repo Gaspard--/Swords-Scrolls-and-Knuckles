e34cfba78dcd753f15d54e29070840c8c7c12e39
Sheep <louis.guittard@epitech.eu>
2017-06-16 22:07:01 +0200

    added a speed variable

--- resources/scripts/pythonModule.py
+++ resources/scripts/pythonModule.py
@@ -3,11 +3,12 @@ import PyPlugin
 
 class pythonModule():
     def chaseAI(self, entity, evaluater):
+        speed = 0.01
         vec = evaluater.closestPlayer(entity.pos);
         vecx = vec.x()
         vecy = vec.y()
         posx = entity.pos.x()
         posy = entity.pos.y()
         newpos = PyPlugin.Vect(vecx - posx, vecy - posy).normalized()
-        entity.setInput(newpos.x() * 0.01, newpos.y() * 0.01);
+        entity.setInput(newpos.x() * speed, newpos.y() * speed);
     pass
