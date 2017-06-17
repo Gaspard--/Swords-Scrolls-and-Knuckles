bf0e4786334d21de40b091ca0df562ce613131df
Sheep <louis.guittard@epitech.eu>
2017-06-17 05:32:49 +0200

    Added 3 python script for the AIs

--- resources/scripts/pythonModule.py
+++ resources/scripts/pythonModule.py
@@ -2,13 +2,34 @@
 import PyPlugin
 
 class pythonModule():
-    def chaseAI(self, entity, evaluater):
-        speed = 0.01
-        vec = evaluater.closestPlayer(entity.pos);
+    def moveEntityFromVec(self, entity, vec, speed):
         vecx = vec.x()
         vecy = vec.y()
         posx = entity.pos.x()
         posy = entity.pos.y()
         newpos = PyPlugin.Vect(vecx - posx, vecy - posy).normalized()
         entity.setInput(newpos.x() * speed, newpos.y() * speed);
-    pass
+
+    def chasePlayerAI(self, entity, evaluater):
+        vec = evaluater.closestPlayer(entity.pos)
+        self.moveEntityFromVec(entity, vec, 0.01)
+
+    def fleePlayerAI(self, entity, evaluater):
+        vec = evaluater.closestPlayer(entity.pos)
+        dist = PyPlugin.Vect(vec.x() - entity.pos.x(), vec.y() - entity.pos.y()).length2()
+        if (dist < 75):
+            self.moveEntityFromVec(entity, vec, -0.015)
+        else:
+            entity.setInput(0.0, 0.0);
+
+    def chaseEnemyAI(self, entity, evaluater):
+        vec = evaluater.closestEnemy(entity.pos)
+        self.moveEntityFromVec(entity, vec, 0.01)
+
+    def fleeEnemyAI(self, entity, evaluater):
+        vec = evaluater.closestEnemy(entity.pos)
+        dist = PyPlugin.Vect(vec.x() - entity.pos.x(), vec.y() - entity.pos.y()).length2()
+        if (dist < 75):
+            self.moveEntityFromVec(entity, vec, -0.015)
+        else:
+            entity.setInput(0.0, 0.0);
