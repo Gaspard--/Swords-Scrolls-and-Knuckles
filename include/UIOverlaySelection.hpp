8d16ca0a3aa9a3cdfcbe8beb3791f1ebc88e610a
benjamin grange <benjamin.grange@epitech.eu>
2017-06-18 23:35:34 +0200

    Worked on gameplay selection.

--- include/UIOverlaySelection.hpp
+++ include/UIOverlaySelection.hpp
@@ -6,16 +6,28 @@
 # include <functional>
 # include <vector>
 # include <Overlay/OgrePanelOverlayElement.h>
+# include "UIOverlayResource.hpp"
 # include "AnimatedEntity.hpp"
 # include "Renderer.hpp"
 # include "UIButton.hpp"
 # include "UIOverlay.hpp"
 # include "Skins.hpp"
 
+enum class Gameplays : int {
+  KEYBOARD = 0,
+  JOYSTICK = 1,
+  IA = 2,
+  NONE = 3,
+  SIZE = 4,
+};
+
+
 class UIOverlaySelection : public UIOverlay {
 private:
   UIOverlayResource<Ogre::PanelOverlayElement> bg;
   size_t selected;
+  std::vector<UIOverlayResource<Ogre::TextAreaOverlayElement>> gameplaysTxt;
+  std::vector<Gameplays> gameplays;
   std::vector<AnimatedEntity> heroes;
   std::vector<int> skins;
   Ogre::Node *cameraNode;
@@ -23,6 +35,7 @@ private:
   static constexpr Ogre::Real const SELECTIONBUTTON_WIDTH = 261.f / Game::WIDTH;
   static constexpr Ogre::Real const SELECTIONBUTTON_HEIGHT = 77.f / Game::HEIGHT;
   static Skins::Skin HEROES_SKINS[4][2];
+  static char const *GAMEPLAYS_NAME[];
 
   void setHeroDefault(AnimatedEntity &ae, size_t);
 
@@ -38,6 +51,8 @@ public:
   void updateUI(Ogre::Real x);
   void changeSkin(Renderer &, bool);
   void changeSelection(bool);
+  void renderGameplayName(size_t);
+  void changeGameplay(bool b);
 };
 
 #endif // !UI_OVERLAY_SELECTION_HPP
--- source/SceneSelection.cpp
+++ source/SceneSelection.cpp
@@ -27,16 +27,29 @@ void SceneSelection::resetSceneCallbacks(Renderer &r) {
       uiSelection.changeSelection(false);
     }
   });
+  auto const changeGameplayUp([this](bool b, size_t = 0) {
+    if (!b) {
+      uiSelection.changeGameplay(true);
+    }
+  });
+  auto const changeGameplayDown([this](bool b, size_t = 0) {
+    if (!b) {
+      uiSelection.changeGameplay(false);
+    }
+  });
 
   InputCallbacks::clearAllCallbacks();
   uiSelection.resetUICallbacks();
-  Keyboard::getKeyboard().registerCallback(OIS::KC_UP, changeSkinUp);
-  Joystick::registerGlobalCallback(joystickState::JS_RUP, changeSkinUp);
-  Keyboard::getKeyboard().registerCallback(OIS::KC_DOWN, changeSkinDown);
-  Joystick::registerGlobalCallback(joystickState::JS_RDOWN, changeSkinDown);
+  Keyboard::getKeyboard().registerCallback(OIS::KC_SPACE, changeSkinUp);
+  Keyboard::getKeyboard().registerCallback(OIS::KC_UP, changeGameplayUp);
+  Keyboard::getKeyboard().registerCallback(OIS::KC_DOWN, changeGameplayDown);
   Keyboard::getKeyboard().registerCallback(OIS::KC_LEFT, changeSelectionDown);
-  Joystick::registerGlobalCallback(joystickState::JS_RLEFT, changeSelectionDown);
   Keyboard::getKeyboard().registerCallback(OIS::KC_RIGHT, changeSelectionUp);
+  Joystick::registerGlobalCallback(joystickState::JS_RT, changeSkinUp);
+  Joystick::registerGlobalCallback(joystickState::JS_LT, changeSkinDown);
+  Joystick::registerGlobalCallback(joystickState::JS_RUP, changeGameplayUp);
+  Joystick::registerGlobalCallback(joystickState::JS_RDOWN, changeGameplayDown);
+  Joystick::registerGlobalCallback(joystickState::JS_RLEFT, changeSelectionDown);
   Joystick::registerGlobalCallback(joystickState::JS_RRIGHT, changeSelectionUp);
 }
 
