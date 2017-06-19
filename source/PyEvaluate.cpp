e1cc824584b7daac9ed5625bf9e312b87ffd2431
Sheep <louis.guittard@epitech.eu>
2017-06-19 18:02:12 +0200

    Added a verification for closestX functions of PyEvaluate

--- source/PyEvaluate.cpp
+++ source/PyEvaluate.cpp
@@ -8,18 +8,22 @@ PyEvaluate::PyEvaluate(std::vector<Player> &players, std::vector<Enemy> &enemies
 
 Vect<2u, double> PyEvaluate::closestPlayer(Vect<2u, double> pos) const
 {
-  return ((*std::min_element(players.begin(), players.end(),
+  auto it (std::min_element(players.begin(), players.end(),
     [pos](Player const &a, Player const &b)
       {
         return ((pos - a.pos).length2() < (pos - b.pos).length2() && (pos - a.pos).length2() != 0);
-      })).pos);
+      }));
+
+  return (it == players.end() ? pos : (*it).pos);
 }
 
 Vect<2u, double> PyEvaluate::closestEnemy(Vect<2u, double> pos) const
 {
-  return ((*std::min_element(enemies.begin(), enemies.end(),
+  auto it (std::min_element(enemies.begin(), enemies.end(),
     [pos](Enemy const &a, Enemy const &b)
       {
         return ((pos - a.pos).length2() < (pos - b.pos).length2() && (pos - a.pos).length2() != 0);
-      })).pos);
+    }));
+
+  return (it == enemies.end() ? pos : (*it).pos);
 }
